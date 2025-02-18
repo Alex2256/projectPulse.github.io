63a4ccb531133a568c43988fe1bd6714a8d08c9f
Alex <alexshura988@gmail.com>
2025-02-18 19:08:23 +0200

    tab adaptation

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('touchstart', {capture: true});
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/chevron_left_solid.svg"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/chevron_right_solid.svg"></img></button>',
+        
+        responsive: [
+            {
+            breakpoint: 992,
+              settings: { 
+                autoplay: true,
+                autoplaySpeed: 3000,
+                arrows: false
+            }
+        }]        
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('.container').find('div.catalog__content').removeClass('catalog__content_active')
+          .eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    $('.catalog-item__link').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+        })
+    });
+
+    $('.catalog-item__back').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    })
+});
