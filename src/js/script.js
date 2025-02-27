6b2815e23b35bd07aedc5b053463bfa06129e127
Alex <alexshura988@gmail.com>
2025-02-27 21:08:01 +0200

    creating modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,9 @@
-document.addEventListener('touchstart', {capture: true});
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/chevron_left_solid.svg"></img></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/slider/chevron_right_solid.svg"></img></button>',
-        
         responsive: [
             {
             breakpoint: 992,
@@ -33,11 +31,25 @@ $(document).ready(function(){
         })
     });
 
-    $('.catalog-item__back').each(function(i) {
-        $(this).on('click', function(e) {
+    $('.catalog-item__back').each(function (i) {
+        $(this).on('click', function (e) {
             e.preventDefault();
             $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
         })
-    })
+    });
+    // modal
+    $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+       $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+    });
+
+    $('button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 });
